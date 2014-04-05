e36e21790d431ad486952282176c7cdb4f2ddab3
kavare <donmoma@gmail.com>
2014-04-05 20:00:11 +0800

    Sidebar Transition Complete

--- js/main.js
+++ js/main.js
@@ -1,1 +1,23 @@
 
+$(function(){
+	$('.header-container').hover(		
+	function(event) {
+		var $this = $(this),
+			$main = $('.main-container');		
+
+		// $this.css('background-color', 'navy');
+		$this.addClass('is-expanded');
+		$main.addClass('is-translated');
+		event.stopProporgation;
+	}, 
+	function() {
+		var $this = $(this),
+			$main = $('.main-container');
+
+		// $this.css('background-color', 'violet');
+		$this.removeClass('is-expanded');
+		$main.removeClass('is-translated');
+		event.stopProporgation; 
+	});
+});
+
