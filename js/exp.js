7d8efa2af590cf946576a602d5db6f4a0a773c59
kavare <donmoma@gmail.com>
2014-04-08 03:06:16 +0800

    All Complete!
    Finally….

---
+++ js/exp.js
@@ -0,0 +1,36 @@
+//Check the window width
+var sWidth = $(window).width(),
+    linrOrient,
+    linrStyle;
+
+if (sWidth > 1024) {
+    linrOrient = 'horizontal';
+    linrStyle = 'css/vendor/Timelinr/horizontal.css';
+} else {
+    linrOrient = 'vertical';
+    linrStyle = 'css/vendor/Timelinr/vertical.css';
+}
+
+//Customized Timelinr Setting   
+$(function(){
+    $().timelinr({
+        orientation:    linrOrient,
+        issuesSpeed:    300,
+        datesSpeed:     100,
+        arrowKeys:      'true',
+        startAt:        1,
+        // autoPlay:       'true',
+        autoPlayPause:  5000 
+    });
+});
+
+//Changing Stylesheet according to device-width
+function includeLinkStyle(url) {
+    var link = document.createElement('link');
+    link.rel = 'stylesheet';
+    link.type = 'text/css';
+    link.href = url;
+    document.getElementsByTagName('head')[0].appendChild(link);
+}
+
+includeLinkStyle(linrStyle);
