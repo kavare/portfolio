fd4a6a7efede2ec9fadc5298bd4d53afce4641f6
kavare <donmoma@gmail.com>
2014-04-07 11:45:44 +0800

    Skillset Complete

---
+++ js/vendor/dribble-menu.js
@@ -0,0 +1,20 @@
+//Revised from Matthew Van Dusen @ Codepen
+$(function () {
+	$('div.dribble-menu').find('li').on('click', function () {
+		var $this = $(this);
+		$this.siblings().removeClass('selected');
+		$this.addClass('selected');
+		$('div.dribble-bottoms').hide();
+		$('#' + $this.attr('data-id')).slideDown(800);
+	});
+    	
+	$('span.toggle').on('click', function () {
+		var $this = $(this);
+		$this.find('span.toggle_inner').toggleClass('on').closest('div.item').toggleClass('on');
+	});
+
+	//default "on" settings
+	$('[data-id="HTML5"]').click();
+	$('[data-skill="able"').addClass('on').find('.toggle_inner').addClass('on');
+
+});
