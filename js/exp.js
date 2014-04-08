816234fc1cb3c77910d46b794be407aec86fd25c
kavare <donmoma@gmail.com>
2014-04-08 17:48:27 +0800

    final trial
    change $().width to screen.width

--- js/exp.js
+++ js/exp.js
@@ -1,9 +1,8 @@
-//Check the window width
-var sWidth = $(window).width(),
-    linrOrient,
+// Check the window width
+var linrOrient,
     linrStyle;
 
-if (sWidth >= 1024) {
+if (screen.width >= 1024) {
     linrOrient = 'horizontal';
     linrStyle = 'css/vendor/Timelinr/horizontal.css';
 } else {
@@ -11,20 +10,9 @@ if (sWidth >= 1024) {
     linrStyle = 'css/vendor/Timelinr/vertical.css';
 }
 
-//Changing Stylesheet according to device-width
-function includeLinkStyle(url) {
-    var link = document.createElement('link');
-    link.rel = 'stylesheet';
-    link.type = 'text/css';
-    link.href = url;
-    document.getElementsByTagName('head')[0].appendChild(link);
-}
-
-includeLinkStyle(linrStyle);
-
 //Customized Timelinr Setting   
 $(function(){
-    $().timelinr({
+    $().timelinr({        
         orientation:    linrOrient,
         issuesSpeed:    300,
         datesSpeed:     100,
@@ -34,3 +22,13 @@ $(function(){
     });
 });
 
+//Changing Stylesheet according to device-width    
+function includeLinkStyle(url) {
+        var link = document.createElement('link');
+        link.rel = 'stylesheet';
+        link.type = 'text/css';
+        link.href = url;
+        document.getElementsByTagName('head')[0].appendChild(link);
+}
+
+includeLinkStyle(linrStyle);
